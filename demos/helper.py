16519bd7b538c5f6a8ecde0e26464e5b568f8979
bzitko <bzitko@pmfst.hr>
2022-11-30 22:23:42 +0100

    a04 pretrained & ostatak

--- demos/helper.py
+++ demos/helper.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from IPython.display import display, Markdown
 
 def tensor2md(tensor, round=3, latex=False):
@@ -8,6 +9,8 @@ def tensor2md(tensor, round=3, latex=False):
         num = tensor.item()
         if isinstance(num, int):
             return str(num)
+        if isinstance(num, str):
+            return num
         return f"{float(num):.{round}}"
 
     txt = r"\begin{bmatrix} "
@@ -17,26 +20,25 @@ def tensor2md(tensor, round=3, latex=False):
         for i in range(m):
             row = []
             for j in range(n):
-                row.append(tensor2md(tensor[i, j]))
+                row.append(tensor2md(tensor[i, j], round=round))
             rows.append(" & ".join(row))
         txt += r" \\ ".join(rows)
     elif s % 2 == 1:
         n = tensor.shape[0]
         row = []
         for i in range(n):
-            row.append(tensor2md(tensor[i]))
+            row.append(tensor2md(tensor[i], round=round))
         txt += r" & ".join(row) 
     txt += r"\end{bmatrix}"
     if latex:
         txt = "$" + txt + "$"
     return txt
 
-
-def look(*args, end=" "):
+def look(*args, round=3, end=" "):
     txt = []
     for arg in args:
-        if isinstance(arg, torch.Tensor):
-            txt.append(tensor2md(arg, latex=True))
+        if isinstance(arg, (torch.Tensor, np.ndarray)):
+            txt.append(tensor2md(arg, round=round, latex=True))
         else:
             txt.append(arg)
     display(Markdown(end.join(txt)))  
